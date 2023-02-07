3b136bd07bf4fa8a09a62eb188b046e6f31d5cce
Jórdan Luiz Bisato <jordanbisato@hotmail.com>
2023-02-07 11:23:55 -0300

    feat: add new methods to reset and update

--- ti.highcharts/controllers/widget.js
+++ ti.highcharts/controllers/widget.js
@@ -1,9 +1,12 @@
-function loadChart(data, pic) {
+function loadChart(data, pic, chartTop, callback) {
+	if (chartTop) {
+		$.chartView.top = chartTop;
+	}
 	var url, plotChart;
+
 	var takePicture = (pic != null) ? pic : false;
 
 	function saveImage() {
-		Ti.API.info('savedImage')
 		var imageView = Ti.UI.createImageView({
 			image: $.chartView.toImage()
 		});
@@ -25,12 +28,50 @@ function loadChart(data, pic) {
 
 	url = WPATH('/html/chart.html');
 	plotChart = 'plotChart(' + JSON.stringify(data) + ', ' + takePicture + ')';
+	// plotChart = 'plotChart(' + JSON.stringify(data) + ')';
 	$.chartView.height = Ti.UI.SIZE;
 	$.chartView.visible = true;
+	$.chartWebView.html = "<html><head><meta name='viewport' content='user-scalable=no'></head></html>";
 	$.chartWebView.url = url;
 	$.chartWebView.addEventListener('load', function() {
-		$.chartWebView.evalJS(plotChart);
+		$.chartWebView.evalJS(plotChart, function() {
+			callback && callback();
+		});
+	});
+}
+
+function resetChart() {
+	$.chartWebView.evalJS('resetChart()');
+}
+
+function stockChart(data, chartTop, callback) {
+	if (chartTop) {
+		$.chartView.top = chartTop;
+	}
+	var url, stockChart;
+
+	url = WPATH('/html/chart.html');
+	stockChart = 'stockChart(' + JSON.stringify(data) + ')';
+	$.chartView.height = Ti.UI.SIZE;
+	$.chartView.visible = true;
+	$.chartWebView.html = "<html><head><meta name='viewport' content='user-scalable=no'></head></html>";
+	$.chartWebView.url = url;
+	$.chartWebView.addEventListener('load', function() {
+		$.chartWebView.evalJS(stockChart, function(e) {
+			Ti.API.info('eval: ' + JSON.stringify(e));
+			callback && callback();
+		});
+	});
+}
+
+function updateChart(data, callback) {
+	// Ti.API.info('updateChart: ' + JSON.stringify(data));
+	$.chartWebView.evalJS('updateChart(' + JSON.stringify(data) + ')', function(e) {
+		callback && callback();
 	});
 }
 
 exports.loadChart = loadChart;
+exports.stockChart = stockChart;
+exports.resetChart = resetChart;
+exports.updateChart = updateChart;
